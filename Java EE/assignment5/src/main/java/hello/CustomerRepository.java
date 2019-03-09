466bbabf50c19fd9abbf9f5cefb7641391406505
c0719943 <c0719943@SBA210619.lambton.on.ca>
2019-03-08 20:02:10 -0500

    first Commit

---
+++ Java EE/assignment5/src/main/java/hello/CustomerRepository.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ *
+ * @author c0719943
+ */
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+}
